97155fc99eb259c8cee79ea21036202ca9bde616
MelaRossa_10 <ikarpach@yandex.ru>
2021-02-23 21:14:13 +0300

    lesson

--- src/OOP/All_Lessons/FIVE/TestHashSet.java
+++ src/OOP/All_Lessons/FIVE/TestHashSet.java
@@ -3,24 +3,18 @@ package OOP.All_Lessons.FIVE;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class TestHashSet {
     public static void main(String[] args) {
-        Set hashSet = new LinkedHashSet<>();
+        Set hashSet = new TreeSet();
         int [] array  = new int [10];
-        hashSet.add("1");
-        hashSet.add("2");
-        hashSet.add("3");
-        hashSet.add("4");
-        hashSet.add("5");
-        hashSet.add("5");
-        hashSet.add("5");
-        hashSet.add("5");
-        hashSet.add("5");
-        hashSet.add(5);
-        hashSet.add("Five");
+        hashSet.add("a");
+        hashSet.add("c");
+        hashSet.add("d");
+        hashSet.add("b");
 
-        hashSet.add(array);
+
 
         System.out.println(hashSet);
     }
