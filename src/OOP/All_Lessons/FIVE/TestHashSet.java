362b293e04c01d3b6840c8b32d03a11fb62a7eae
MelaRossa_10 <ikarpach@yandex.ru>
2021-02-23 21:01:31 +0300

    lesson

--- src/OOP/All_Lessons/FIVE/TestHashSet.java
+++ src/OOP/All_Lessons/FIVE/TestHashSet.java
@@ -1,11 +1,12 @@
 package OOP.All_Lessons.FIVE;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class TestHashSet {
     public static void main(String[] args) {
-        Set hashSet = new HashSet<>();
+        Set hashSet = new LinkedHashSet<>();
         int [] array  = new int [10];
         hashSet.add("1");
         hashSet.add("2");
