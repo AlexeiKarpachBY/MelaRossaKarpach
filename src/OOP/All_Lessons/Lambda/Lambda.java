0d750fa3a44e42bd5b132eac0ff7afd1457ff632
MelaRossa_10 <ikarpach@yandex.ru>
2021-03-04 21:10:04 +0300

    fxnc

--- src/OOP/All_Lessons/Lambda/Lambda.java
+++ src/OOP/All_Lessons/Lambda/Lambda.java
@@ -1,4 +1,21 @@
 package OOP.All_Lessons.Lambda;
 
+import java.util.function.Predicate;
+
 public class Lambda {
+
+    public static void main(String[] args) {
+        Operationable operation;
+        operation = (x, y) -> x * y;
+        int result = operation.calculate(2, 2);
+
+        System.out.println(result);
+
+        Predicate<Integer> isPositive = x -> x > 0;
+        System.out.println(isPositive.test(2));
+        System.out.println(isPositive.test(-1));
+
+        
+    }
+
 }
---
+++ src/OOP/All_Lessons/Lambda/Operationable.java
@@ -0,0 +1,7 @@
+package OOP.All_Lessons.Lambda;
+
+public interface Operationable {
+
+    int calculate (int x, int y);
+
+}
